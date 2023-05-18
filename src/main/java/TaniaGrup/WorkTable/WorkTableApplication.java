4677e26bf6c8783e47b0ffdcae6450a4715da1b6
EfrosEugeniu-md <efrosevgenij@gmail.com>
2023-05-18 10:15:01 +0300

    jhn

--- src/main/java/TaniaGrup/WorkTable/WorkTableApplication.java
+++ src/main/java/TaniaGrup/WorkTable/WorkTableApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkTableApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WorkTableApplication.class, args);
 	}
 
